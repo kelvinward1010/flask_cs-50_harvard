50cc624025e82d530d6005da91bebdce450ee05a
Kelvin Ward <kelvinward1010@gmail.com>
2023-12-26 17:02:35 +0700

    first commit

---
+++ hello/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', "POST"])
+def index():
+    if request.method == 'POST':
+        name = request.form.get('name', 'world')
+        return render_template("greet.html", name=name)
+    return render_template("index.html")
+
+# @app.route('/greet', methods=['POST'])
+# def greet():
+#     name = request.form.get('name', 'world')
+#     return render_template("greet.html", name=name)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
